c9a14c796158f1cf61a378a0125061c4b6dff358
achrafwe <kalchicharaf@gmail.com>
2024-04-07 08:56:28 +0200

    front end

--- Frontend/Wolfi/src/app/home/home.page.ts
+++ Frontend/Wolfi/src/app/home/home.page.ts
@@ -13,12 +13,12 @@ export class HomePage {
 
   roles = [
     { 
-      imageUrl: 'https://static.wikia.nocookie.net/loupgaroumal/images/d/d6/Carte_SimpleVillaegois.png/revision/latest/scale-to-width-down/185?cb=20210104170925&path-prefix=fr',
+      imageUrl: 'https://i.imgur.com/R9YJymr.png',
       name: 'Villageois',
       description: 'Le villageois est un personnage qui incarne l habitant basique d un village. Son rôle est de découvrir l identité des loups-garous et de les éliminer avant qu ils ne tuent tous les villageois. Il gagne lors ce que tous les loups garous sont morts. Le villageois n a aucun pouvoir spécial, si ce n est de voter au conseil du village contre celui qu il suspecte être loup garou.'
     },
     { 
-      imageUrl: 'https://static.wikia.nocookie.net/loupgaroumal/images/1/1e/Carte2.png/revision/latest/scale-to-width-down/185?cb=20210104171045&path-prefix=fr',
+      imageUrl: 'https://i.imgur.com/qklKzwl.png',
       name: 'Loup-Garou',
       description: 'Le loup-garou incarne l un des rôles de l équipe des loups-garous. Il connaît l identité des autres loups-garous et doit essayer de tuer tous les villageois sans se faire découvrir. Il se réunit chaque nuit avec les autres loups-garous pour décider de leur victime. Il gagne si tout le village est éliminé.'
     },
